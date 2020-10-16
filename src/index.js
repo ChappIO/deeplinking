3efb5694d6d546a22f44445e48cfcadfd4e4d6f0
Thomas Biesaart <thomas@infi.nl>
2020-10-16 16:09:04 +0200

    Enable service worker

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './App';
 import './style.scss';
+import {register} from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
+register();
